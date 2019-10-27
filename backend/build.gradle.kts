f5e2320b18bb3aa1c3aadd8b9aaf7539ea318c69
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2019-10-27 18:19:55 +0100

    Move test folder below src and change gradle script so tests can be run

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,8 +1,25 @@
+import org.gradle.api.internal.tasks.testing.junitplatform.JUnitPlatformTestFramework
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktor_version = "1.2.4"
 
+apply {
+	plugin("org.junit.platform.gradle.plugin")
+}
+
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+	dependencies {
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+		classpath("org.junit.platform:junit-platform-gradle-plugin:1.1.0")
+	}
+}
+
 plugins {
+	id("idea")
+	id("java")
 	id("org.springframework.boot") version "2.2.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.50"
@@ -27,6 +44,8 @@ dependencies {
 	implementation(group = "io.ktor", name = "ktor-client-apache", version = ktor_version)
 	implementation(group = "org.jsoup", name = "jsoup", version = "1.12.1")
 
+	testImplementation(group ="com.github.tomakehurst", name = "wiremock-jre8", version = "2.25.1")
+	testImplementation(group ="org.junit.jupiter", name = "junit-jupiter-api", version = "5.5.2")
 	testImplementation(group ="org.springframework.boot", name = "spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
@@ -34,6 +53,9 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		events("passed", "skipped", "failed")
+	}
 }
 
 tasks.withType<KotlinCompile> {
