d9ffb06a2e366259bd2c352e0cc4cd4b269c0f47
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2019-10-31 17:05:53 +0100

    Use different base URL for production and development
    Resolves #11

--- frontend/src/Api.kt
+++ frontend/src/Api.kt
@@ -2,8 +2,11 @@ import kotlinx.coroutines.await
 import kotlin.browser.window
 import Models.Media
 
+external val process: dynamic
+
 suspend fun fetchAvailableMedias(user: String, pass: String): Array<Media> {
-    val responsePromise = window.fetch("http://localhost:8080/api/media?username=$user&password=$pass")
+    val backendBaseUrl = if (process.env.NODE_ENV == "production") "/api" else "http://localhost:8080/api"
+    val responsePromise = window.fetch("$backendBaseUrl/media?username=$user&password=$pass")
     val response = responsePromise.await()
     val jsonPromise = response.json()
     val json = jsonPromise.await()
