126d4b51532e3c054a68a18d0ff119f6698db8fb
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2019-10-31 20:53:58 +0100

    Pass username and password as headers instead of request params

--- backend/src/main/kotlin/de/theonebrack/merkliste_20/Controller/DefaultController.kt
+++ backend/src/main/kotlin/de/theonebrack/merkliste_20/Controller/DefaultController.kt
@@ -1,26 +1,11 @@
 package de.theonebrack.merkliste_20.Controller
 
-import de.theonebrack.merkliste_20.Models.LoginFormData
 import de.theonebrack.merkliste_20.Models.Media
 import de.theonebrack.merkliste_20.Services.BuecherhallenService
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.features.BrowserUserAgent
-import io.ktor.client.features.cookies.AcceptAllCookiesStorage
-import io.ktor.client.features.cookies.HttpCookies
-import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.client.request.post
-import io.ktor.client.request.url
-import io.ktor.content.TextContent
-import io.ktor.http.ContentType
-import kotlinx.coroutines.runBlocking
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Element
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -31,7 +16,7 @@ class DefaultController(
     val logger = LoggerFactory.getLogger(DefaultController::class.java)
 
     @GetMapping("/api/media")
-    fun get(@RequestParam username: String, @RequestParam password: String): List<Media> {
+    fun get(@RequestHeader("username") username: String, @RequestHeader("password") password: String): List<Media> {
         logger.info("Incoming request for all available media")
         return buecherhallenService.fetchAll(username, password)
     }
--- frontend/src/Api.kt
+++ frontend/src/Api.kt
@@ -1,12 +1,19 @@
-import kotlinx.coroutines.await
 import kotlin.browser.window
+import kotlin.js.json
+import kotlinx.coroutines.await
+import org.w3c.fetch.*
 import Models.Media
 
 external val process: dynamic
 
 suspend fun fetchAvailableMedias(user: String, pass: String): Array<Media> {
     val backendBaseUrl = if (process.env.NODE_ENV == "production") "/api" else "http://localhost:8080/api"
-    val responsePromise = window.fetch("$backendBaseUrl/media?username=$user&password=$pass")
+    val responsePromise = window.fetch("$backendBaseUrl/media",
+            object: RequestInit {
+                override var method: String? = "GET"
+                override var headers: dynamic = json("username" to user, "password" to pass)
+            }
+    )
     val response = responsePromise.await()
     val jsonPromise = response.json()
     val json = jsonPromise.await()
