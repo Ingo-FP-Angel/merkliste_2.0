bdcb4e42e3646af47498cfed416b36ece96c5e25
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2020-03-21 19:36:31 +0100

    Update dependecies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,15 +2,15 @@ import java.nio.charset.StandardCharsets
 import org.apache.tools.ant.filters.ReplaceTokens
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktor_version = "1.2.4"
+val ktor_version = "1.2.6"
 
 plugins {
 	id("idea")
 	id("java")
-	id("org.springframework.boot") version "2.2.0.RELEASE"
+	id("org.springframework.boot") version "2.2.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.3.50"
-	kotlin("plugin.spring") version "1.3.50"
+	kotlin("jvm") version "1.3.70"
+	kotlin("plugin.spring") version "1.3.70"
 }
 
 val appName = "merkliste_20"
@@ -27,7 +27,7 @@ dependencies {
 	implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin")
 	implementation(group = "org.jetbrains.kotlin", name = "kotlin-reflect")
 	implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8")
-	implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.2")
+	implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.5")
 	implementation(group = "io.ktor", name = "ktor-client-core", version = ktor_version)
 	implementation(group = "io.ktor", name = "ktor-client-apache", version = ktor_version)
 	implementation(group = "org.jsoup", name = "jsoup", version = "1.12.1")
@@ -54,6 +54,7 @@ task<Test>("integration") {
 	filter {
 		includeTestsMatching("*IT")
 	}
+	dependsOn("test")
 }
 
 tasks.withType<KotlinCompile> {
