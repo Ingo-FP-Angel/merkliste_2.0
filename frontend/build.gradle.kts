3019962284644a7be8f990d60174e8fe419d8038
Ingo-FP-Angel <ingo_fp_angel@gmx.de>
2020-12-05 20:52:41 +0100

    Increase version for new release

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -2,8 +2,13 @@ plugins {
     id("com.moowork.node") version "1.3.1" apply true
 }
 
+node {
+    download = true
+}
+
 tasks {
     register<GradleBuild>("buildApplication") {
+        group = "build"
         tasks = listOf("yarn_install", "yarn_build")
     }
 }
